22b5dfad3f1ff2b7036863562ee0b6148f44db8a
Ivaylo Atanasov <ivailo.atanasov93@gmail.com>
2022-04-27 09:25:35 +0100

    Flatten the JSON with external package

--- extractor.py
+++ extractor.py
@@ -8,6 +8,8 @@ from os import listdir
 class Extractor:
     def __init__(self, path):
         self.folder_path = path
+        self.table_names = set()
+        self.table_row_names = dict()
 
     def get_info(self):
         files = self.get_files()
@@ -17,7 +19,14 @@ class Extractor:
                 content = json.loads(json_file.read())['entry']
                 for json_object in content:
                     flatten_object = flatten(json_object)
-                    print(flatten_object)
+                    table_name = flatten_object['resource_resourceType']
+                    self.table_names.add(table_name)
+                    if table_name not in self.table_row_names.keys():
+                        self.table_row_names[table_name] = set()
+                    for key in flatten_object.keys():
+                        self.table_row_names[table_name].add(key)
+        print(f'HERE ARE THE TABLE NAMES: {self.table_names}')
+        [print(f'THIS IS A SINGLE KEY: {item}') for item in self.table_row_names.items()]
 
     def get_files(self):
         return [file for file in listdir(folder_path)]
